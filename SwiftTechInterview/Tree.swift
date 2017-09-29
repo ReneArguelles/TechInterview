3d69e8a8e0a11574b0c11e5f5c735f201e4bb3ed
René Argüelles <rarguelles@luxoft.com>
2017-09-29 12:36:36 -0500

    Project setup

---
+++ SwiftTechInterview/Tree.swift
@@ -0,0 +1,17 @@
+//
+//  Tree.swift
+//  SwiftTechInterview
+//
+//  Created by Luxoft on 9/29/17.
+//  Copyright © 2017 Rene Argüelles. All rights reserved.
+//
+
+import Foundation
+
+class Tree {
+    
+    func getTrue() -> Bool {
+        return true
+    }
+    
+}
